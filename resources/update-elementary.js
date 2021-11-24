6c6ed63706dc46d6db00901f64aad9462365c355
Arifian - Lead ProGen <arifian@MB000036-MacBook-Air.local>
2021-11-24 14:38:12 +0700

    update some db script

--- resources/update-elementary.js
+++ resources/update-elementary.js
@@ -3,4 +3,5 @@ db.problems.updateMany({"difficulty": "elementary"}, {$set: {"difficulty": "Elem
 db.problems.updateMany({"difficulty": "easy"}, {$set: {"difficulty": "Easy"}})
 db.problems.updateMany({"difficulty": "medium"}, {$set: {"difficulty": "Medium"}})
 db.problems.updateMany({"difficulty": "hard"}, {$set: {"difficulty": "Hard"}})
-
+db.problems.updateMany({"_id": NumberLong(118)}, {$set: {"restricted" : [ "map" ]}})
+db.problems.updateMany({"_id": NumberLong(116)}, {$unset: {"restricted" : ""}})
